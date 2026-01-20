fcd8641cc595a6e09ee9fafd972a12a67ec2e376
Øivind Amble <oivind.amble@kartverket.no>
2026-01-20 12:56:26 +0100

    Initial Commit

---
+++ design-tokens-build/types.d.ts
@@ -0,0 +1,17 @@
+/* build: v1.9.0 */
+import type {} from '@digdir/designsystemet-types';
+
+// Augment types based on theme
+declare module '@digdir/designsystemet-types' {
+  export interface ColorDefinitions {
+    accent: never;
+    blue: never;
+    neutral: never;
+  }
+  export interface SeverityColorDefinitions {
+    info: never;
+    success: never;
+    warning: never;
+    danger: never;
+  }
+}
