7ec895fe390969a9a4c22884d5248ded1129b4dd
Øivind Amble <oivind.amble@kartverket.no>
2026-01-20 12:56:26 +0100

    Initial Commit

---
+++ design-tokens-build/types.d.ts
@@ -0,0 +1,33 @@
+/* build: v1.11.0 */
+import type {} from '@digdir/designsystemet-types';
+
+// Augment types based on theme
+declare module '@digdir/designsystemet-types' {
+  export interface ColorDefinitions {
+    'brand-green': never;
+    'brand-blue': never;
+    'brand-lightblue': never;
+    'brand-grey': never;
+    'brand-white': never;
+    'brand-yellow': never;
+    'brand-orange': never;
+    'brand-red': never;
+    'brand-purple': never;
+    'ui-1': never;
+    'ui-2': never;
+    'ui-3': never;
+    'ui-4': never;
+    'ui-5': never;
+    'ui-6': never;
+    'ui-7': never;
+    'ui-8': never;
+    'ui-9': never;
+    neutral: never;
+  }
+  export interface SeverityColorDefinitions {
+    info: never;
+    success: never;
+    warning: never;
+    danger: never;
+  }
+}
