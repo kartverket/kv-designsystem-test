dfdb3a33c434f94c59faf8e5a2f0ce56a6014814
Clara Zweigel <clara.zweigel@kartverket.no>
2026-02-09 11:59:50 +0100

    restructured design token colors

--- design-tokens-build/types.d.ts
+++ design-tokens-build/types.d.ts
@@ -1,27 +1,12 @@
-/* build: v1.11.0 */
+/* build: v1.11.1 */
 import type {} from '@digdir/designsystemet-types';
 
 // Augment types based on theme
 declare module '@digdir/designsystemet-types' {
   export interface ColorDefinitions {
-    'brand-green': never;
-    'brand-blue': never;
-    'brand-lightblue': never;
-    'brand-grey': never;
-    'brand-white': never;
-    'brand-yellow': never;
-    'brand-orange': never;
-    'brand-red': never;
-    'brand-purple': never;
-    'ui-1': never;
-    'ui-2': never;
-    'ui-3': never;
-    'ui-4': never;
-    'ui-5': never;
-    'ui-6': never;
-    'ui-7': never;
-    'ui-8': never;
-    'ui-9': never;
+    accent: never;
+    'support-1': never;
+    'support-2': never;
     neutral: never;
   }
   export interface SeverityColorDefinitions {
