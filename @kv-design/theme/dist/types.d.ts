be2d965c77c47837922b7ff81087ff87508b3d1e
Clara Zweigel <clara.zweigel@kartverket.no>
2026-02-18 10:06:07 +0100

    updated .gitignore to not ignore dist

---
+++ @kv-design/theme/dist/types.d.ts
@@ -0,0 +1,18 @@
+/* build: v1.11.1 */
+import type {} from '@digdir/designsystemet-types';
+
+// Augment types based on theme
+declare module '@digdir/designsystemet-types' {
+  export interface ColorDefinitions {
+    accent: never;
+    'support-1': never;
+    'support-2': never;
+    neutral: never;
+  }
+  export interface SeverityColorDefinitions {
+    info: never;
+    success: never;
+    warning: never;
+    danger: never;
+  }
+}
