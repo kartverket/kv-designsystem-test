b67983bf2c2c42ba0c34045c5fcb45225b0382e2
Clara Zweigel <clara.zweigel@kartverket.no>
2026-02-19 14:08:18 +0100

    added documentation for breadcrumbs

--- @kv-design/react/src/components/breadcrumbs/Breadcrumbs.stories.tsx
+++ @kv-design/react/src/components/breadcrumbs/Breadcrumbs.stories.tsx
@@ -11,11 +11,10 @@ type Story = StoryObj<typeof meta>;
 
 export const Preview: Story = {
     args: {
-        // children: 'Breadcrumbs',
         'aria-label':'Du er her:',
     },
     render: (args) => (
-        <>
+        // TODO: In storybook-view code the outermost Breadcrumbs is swapped with React.ForwardRef
         <Breadcrumbs {...args}>
             <Breadcrumbs.Link href='#' aria-label='Tilbake til Nivå 3'>
                 Nivå 3
@@ -35,6 +34,47 @@ export const Preview: Story = {
                 </Breadcrumbs.Item>
             </Breadcrumbs.List>
         </Breadcrumbs>
-        </>
     ),
 };
+
+export const ListOnly: Story = {
+    args: {
+        'aria-label':'Du er her:',
+    },
+    render: (args) => (
+    <Breadcrumbs {...args}>
+      <Breadcrumbs.List>
+        <Breadcrumbs.Item>
+          <Breadcrumbs.Link href="#">Kartverket</Breadcrumbs.Link>
+        </Breadcrumbs.Item>
+        <Breadcrumbs.Item>
+          <Breadcrumbs.Link href="#">Til lands</Breadcrumbs.Link>
+        </Breadcrumbs.Item>
+        <Breadcrumbs.Item>
+          <Breadcrumbs.Link href="#">Stadnamn</Breadcrumbs.Link>
+        </Breadcrumbs.Item>
+        <Breadcrumbs.Item>
+          <Breadcrumbs.Link href="#">Søk stadnamn i kart</Breadcrumbs.Link>
+        </Breadcrumbs.Item>
+      </Breadcrumbs.List>
+    </Breadcrumbs>
+    ),
+};
+
+export const BackOnly: Story = {
+    args: {
+        'aria-label':'Du er her:',
+    },
+    render: (args) => (
+        <Breadcrumbs>
+        <Breadcrumbs.Link
+            aria-label="Tilbake til Stadnamn"
+            href="#"
+        >
+            Stadnamn
+        </Breadcrumbs.Link>
+        </Breadcrumbs>
+    ),
+};
+
+
