317fee3eaac2d3d89306795ee27e827cac73e333
Clara Zweigel <clara.zweigel@kartverket.no>
2026-02-19 12:43:39 +0100

    added documentation for badge

--- @kv-design/react/src/components/avatar/Avatar.stories.tsx
+++ @kv-design/react/src/components/avatar/Avatar.stories.tsx
@@ -1,5 +1,8 @@
 import type { Meta, StoryObj } from '@storybook/react-vite';
 import { Avatar } from './Avatar';
+import { Dropdown } from '../dropdown/Dropdown';
+import { Badge } from '../badge/Badge';
+import { Link } from '../link/Link';
 
 const meta = {
   component: Avatar,
@@ -20,8 +23,118 @@ export const Preview: Story = {
     <>
     <Avatar {...args}>
       {/* TODO: Remove this? Add an image or keep it with default icon? */}
-      {/* <img src={profileImage} alt="" /> */}
+      {/* <img src={profileImage} alt='' /> */}
     </Avatar>
     </>
+  ),
+};
+
+// TODO: add spacing between icons
+export const Content: Story = {
+  args: {
+    'aria-label': 'Ola Nordmann',
+  },
+  render: (args) => (
+    <>
+    <Avatar {...args} />
+    {/* TODO: fix so avatar is aligned with the others */}
+    <Avatar {...args} initials='ON'/>
+    {/* TODO: add icon, briefcase? */}
+    <Avatar {...args} /> 
+    {/* TODO: add image */}
+    <Avatar {...args} />
+    </>
+  ),
+}
+
+export const Sizes: Story = {
+  args: {
+    'aria-label': 'Ola Nordmann'
+  },
+  render: (args) => (
+    <>
+    <Avatar {...args} data-size='xs' aria-label='extra small' initials='xs' />
+    <Avatar {...args} data-size='sm' aria-label='small' initials='sm' />
+    <Avatar {...args} data-size='md' aria-label='medium' initials='md' />
+    <Avatar {...args} data-size='lg' aria-label='large' initials='lg' />
+    </>
+  )
+}
+
+export const ColorVariants: Story = {
+  args: {
+    'aria-label': 'Ola Nordmann'
+  },
+  render: (args) => (
+    <>
+    <Avatar {...args} data-color='neutral' aria-label='color neutral' />
+    <Avatar {...args} data-color='accent' aria-label='color accent' />
+    <Avatar {...args} data-color='support-1' aria-label='color support-1' />
+    <Avatar {...args} data-color='support-2' aria-label='color support-2' />
+    </>
+  )
+}
+
+export const Shapes: Story = {
+  args: {
+    'aria-label': 'Ola Nordmann'
+  },
+  render: (args) => (
+    <>
+    <Avatar {...args} variant='circle' aria-label='variant circle' />
+    <Avatar {...args} variant='square' aria-label='variant square' />
+    <Avatar {...args} variant='circle' aria-label='Ola Nordmann' initials='ON'/>
+    <Avatar {...args} variant='square' aria-label='Ola Nordmann' initials='ON'/>
+    </>
+  )
+}
+
+export const InDropdown: Story = {
+  args: {
+    'aria-label': 'Ola Nordmann'
+  },
+  render: (args) => (
+    <Dropdown.TriggerContext>
+      <Dropdown.Trigger variant='tertiary'>
+        <Avatar aria-hidden='true' data-size='sm' initials='ON' />
+        Ola Nordmann
+      </Dropdown.Trigger>
+      <Dropdown placement='bottom-end' autoPlacement={false} data-size='md'>
+        <Dropdown.List>
+          <Dropdown.Item>
+            <Dropdown.Button>
+              <Badge.Position overlap='circle'>
+                <Badge data-color='danger' data-size='sm'></Badge>
+                <Avatar aria-hidden='true' data-size='xs' initials='ON' />
+              </Badge.Position>
+              Ola Nordmann
+            </Dropdown.Button>
+          </Dropdown.Item>
+          <Dropdown.Item>
+            <Dropdown.Button>
+              <Avatar aria-hidden='true' data-size='xs'>
+                {/* <BriefcaseIcon /> */}
+              </Avatar>
+              Sogndal kommune
+            </Dropdown.Button>
+          </Dropdown.Item>
+        </Dropdown.List>
+      </Dropdown>
+    </Dropdown.TriggerContext>
+  )
+}
+
+export const AsLink: Story = {
+  args: {
+    'aria-hidden': true,
+  },
+  render: (args) => (
+    <Link
+      href='https://www.kartverket.no/'
+      style={{ display: 'flex', gap: 'var(--ds-size-2)', alignItems: 'center' }}
+    >
+      <Avatar {...args} />
+      <span>Ola Nordmann</span>
+    </Link>
   ),
 };
--- @kv-design/react/src/components/badge/Badge.stories.tsx
+++ @kv-design/react/src/components/badge/Badge.stories.tsx
@@ -1,5 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react-vite';
 import { Badge } from './Badge';
+import { Tabs } from '../tabs/Tabs';
 
 const meta = {
   component: Badge,
@@ -15,3 +16,115 @@ export const Preview: Story = {
     maxCount: 9,  
   },
 };
+
+const ColorsMap: {
+  [key: string]: { [key: string]: string };
+} = {
+  neutralBase: {
+    'data-color': 'neutral',
+  },
+  neutralTinted: {
+    'data-color': 'neutral',
+    'data-variant': 'tinted',
+  },
+  dangerBase: {
+    'data-color': 'danger',
+  },
+  dangerTinted: {
+    'data-color': 'danger',
+    'data-variant': 'tinted',
+  },
+  infoBase: {
+    'data-color': 'info',
+  },
+  infoTinted: {
+    'data-color': 'info',
+    'data-variant': 'tinted',
+  },
+  warningBase: {
+    'data-color': 'warning',
+  },
+  warningTinted: {
+    'data-color': 'warning',
+    'data-variant': 'tinted',
+  },
+  successBase: {
+    'data-color': 'success',
+  },
+  successTinted: {
+    'data-color': 'success',
+    'data-variant': 'tinted',
+  },
+};
+
+export const ColorVariants: Story = {
+  args: {  
+  },
+  render: () => (
+    <div
+      style={{
+        display: 'grid',
+        gridTemplateColumns: 'repeat(4, 60px)',
+        gap: 'var(--ds-size-2)',
+        justifyContent: 'center',
+        height: '100%',
+        width: '100%',
+      }}
+    >
+      {Object.entries(ColorsMap).map(([key, value]) => (
+        <Badge key={key} {...value} count={15} maxCount={9} />
+      ))}
+    </div>
+  ),
+};
+
+export const Status: Story = {
+  args: {
+    'data-color': 'danger'
+  },
+  render: (args) => (
+    <div>
+      <Badge.Position data-size='lg'>
+        <Badge {...args} />
+        {/* TODO: add icon */}
+        {/* <FloppydiskFillIcon title="Lagre" /> */}
+      </Badge.Position>
+    </div>
+  )
+}
+
+export const InTabs: Story = {
+  args: {
+  },
+  render: (args) => (
+    <Tabs defaultValue='value1'>
+      <Tabs.List>
+        <Tabs.Tab value='value1'>
+          Favoritter
+          <Badge count={64} maxCount={10} data-color='neutral' />
+        </Tabs.Tab>
+        <Tabs.Tab value='value2'>Arkiv</Tabs.Tab>
+        <Tabs.Tab value='value3'>
+          Nylige
+          <Badge count={2} data-color='neutral' />
+        </Tabs.Tab>
+      </Tabs.List>
+      <Tabs.Panel value='value1'>content 1</Tabs.Panel>
+      <Tabs.Panel value='value2'>content 2</Tabs.Panel>
+      <Tabs.Panel value='value3'>content 3</Tabs.Panel>
+    </Tabs>
+  )
+}
+
+export const Floating: Story = {
+  args: {
+    'data-color': 'danger'
+  },
+  render: (args) => (
+    <Badge.Position placement='top-right'>
+      <Badge data-color='danger' count={2}></Badge>
+      {/* TODO: add icon */}
+      {/* <EnvelopeClosedFillIcon title='Meldinger' style={{ fontSize: '2rem' }} /> */}
+    </Badge.Position>
+  )
+}
