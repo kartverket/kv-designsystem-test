ca5e2d6fe5ce8e05272a2187c833d479af81d0c4
Clara Zweigel <clara.zweigel@kartverket.no>
2026-03-18 12:35:57 +0100

    first draft info and get-started pages

---
+++ @kv-design/react/src/documentation/introduction/Introduction.tsx
@@ -0,0 +1,64 @@
+import { ExternalLinkIcon } from '@navikt/aksel-icons';
+import classes from '../componentCatalog/componentCatalog.module.css';
+import { Card } from '../../components/card/Card';
+import { Link } from '../../components/link/Link';
+import { Heading } from '../../components/typography/heading/Heading';
+import { Paragraph } from '../../components/typography/paragraph/Paragraph';
+
+const pages = [
+    {
+        title: 'Kom i gang som utvikler',
+        href: '/?path=/docs/kom-i-gang-som-utvikler--docs',
+        description: 'En steg for steg guide for hvordan du tar i bruk designsystemet som utvikler.',
+    },
+    {
+        title: 'Kom i gang med design',
+        href: '/?path=/docs/kom-i-gang-med-design--docs',
+        description: 'En guide for hvordan du tar i bruk designsystemet i Figma.',
+    },
+    {
+        title: 'Komponenter',
+        href: '/?path=/docs/komponentoversikt--docs',
+        description: 'Et bibliotek med komponenter, forhåndsvisninger, kildekode og retningslinjer for bruk.',
+    },
+    {
+        title: 'Tokens',
+        href: '/?path=/docs/design-tokens-og-variabler--docs',
+        description: 'Våre design tokens som styrer blant annet farger, størrelser og avstander på tvers av kode og Figma.',
+    },
+    {
+        title: 'Visuell identitet',
+        href: 'https://www.kartverket.no/om-kartverket/kartverkets-identitet',
+        description: 'Bli kjent med Kartverket sin merkevare. Veiledning og retningslinjer til deg som skal ta identiteten i bruk.',
+        external: true,
+    },
+];
+
+
+// TODO: we are reusing .grid from componentCatalog, should we make it more global or make its own?
+// TODO: change name of function??
+export function PagesIndex() {
+    return (
+        <div className={classes.grid} >
+            {pages.map((page) => (
+                <Card key={page.title}>
+                    <Card.Block>
+                        <Heading>
+                            <Link 
+                                href={page.href}
+                                {...(page.external && {
+                                    rel: 'noopener noreferrer',
+                                    target: '_blank',
+                                })}
+                            >
+                                {page.title}
+                                {page.external && <ExternalLinkIcon />}
+                            </Link>
+                        </Heading>
+                        <Paragraph>{page.description}</Paragraph>
+                    </Card.Block>
+                </Card>
+            ))}
+        </ div>
+    )
+};
