591c8e78276927bf6cf4eb01a31b93ff467f8bba
Clara Zweigel <clara.zweigel@kartverket.no>
2026-08-06 09:02:11 +0200

    initial commit

--- @kv-designsystem/react/src/components/header/Header.tsx
+++ @kv-designsystem/react/src/components/header/Header.tsx
@@ -7,13 +7,21 @@ import { HeaderBrand } from './HeaderBrand';
 
 export type HeaderProps = HTMLAttributes<HTMLElement> & {
   /**
+   * Header variant.
+   * @default 'public'
+   */
+  variant?: 'public' | 'internal';
+  /**
    * The name of the application, displayed in the header.
    */
   applicationName?: string;
   /**
-   * The URL applicationName links to. Set to `null` to disable the link.
-   */
-  applicationHref?: string | null;
+    * URL for the application name.
+    *
+    * Required for the internal variant, and when applicationName is provided
+    * for the public variant.
+    */
+  applicationHref?: string;
   /**
    * The maximum width of the header content.
    * Can be any valid CSS width value, e.g. `80rem`, `100%`, etc.
@@ -28,12 +36,20 @@ export type HeaderProps = HTMLAttributes<HTMLElement> & {
 };
 
 export const Header = forwardRef<HTMLElement, HeaderProps>(function Header(
-  { applicationHref, applicationName, children, className, maxWidth = '1296px',  ...rest},
+  { 
+    variant = 'public',
+    applicationHref, 
+    applicationName, 
+    children, 
+    className, 
+    maxWidth = '1296px', 
+    ...rest 
+  },
   ref
 ) {
   const [showHeader, setShowHeader] = useState(true);
   const lastScrollY = useRef(0);
-  
+
   useEffect(() => {
     const handleScroll = () => {
       const currentScrollY = window.scrollY;
@@ -52,20 +68,21 @@ export const Header = forwardRef<HTMLElement, HeaderProps>(function Header(
   }, []);
 
   return (
-    <header 
-      className={cl('header', (showHeader ? 'visible' : 'hidden'), className)} 
+    <header
+      className={cl('header', (showHeader ? 'visible' : 'hidden'), className)}
       style={{ '--kvdsc-header-max-width': maxWidth } as CSSProperties}
-      ref={ref} 
+      ref={ref}
       {...rest}
     >
       <div className='header-container'>
 
         <HeaderBrand
+          variant={variant}
           applicationName={applicationName}
           applicationHref={applicationHref}
         />
 
-        {children && <div className='header-content'>{children}</div> }
+        {children && <div className='header-content'>{children}</div>}
       </div>
     </header>
   )
--- @kv-designsystem/react/src/components/header/HeaderBrand.tsx
+++ @kv-designsystem/react/src/components/header/HeaderBrand.tsx
@@ -4,33 +4,82 @@ import logoIcon from '../../../assets/img/KV_Logo_Icon.svg';
 import { Link } from '../link/Link';
 import { Heading } from '../typography/heading/Heading';
 
-type HeaderBrandProps = {
-  applicationName?: string;
-  applicationHref?: string | null;
+// If applicationName is provided, applicationHref is required
+type PublicBrandProps = 
+ | {
+    variant?: 'public';
+    applicationName?: undefined;
+    applicationHref?: string;
+  }
+  | {
+    variant?: 'public';
+    applicationName: string;
+    applicationHref: string;
+  }
+;
+
+type InternalBrandProps = {
+  variant: 'internal';
+  applicationName: string;
+  applicationHref: string;
 };
 
+type HeaderBrandProps = PublicBrandProps | InternalBrandProps;
+
 export const HeaderBrand = forwardRef<HTMLDivElement, HeaderBrandProps>(
-  function HeaderBrand({ applicationName, applicationHref }, ref) {
+  function HeaderBrand(props, ref) {
     return (
       <div className='header-brand' ref={ref}>
-        <a href='https://kartverket.no'>
-          <img src={logoFull} alt='Kartverket-logo' className='header-logo header-logo--full' />
-          <img src={logoIcon} alt='Kartverket-logo' className='header-logo header-logo--icon' />
-        </a>
-
-        {applicationName && (
-          <>
-            <span className='header-brand-divider' />
-            {applicationHref ? (
-              <Link href={applicationHref}>
-                <Heading data-size='2xs'>{applicationName}</Heading>
-              </Link>
-            ) : (
-              <Heading data-size='2xs'>{applicationName}</Heading>
-            )}
-          </>
-        )}
+        {props.variant === 'internal'
+          ? (
+            <InternalBrand {...props} />
+          ) : (
+            <PublicBrand {...props} />
+          )}
       </div>
     );
   }
 );
+
+function PublicBrand({ applicationName, applicationHref }: PublicBrandProps) {
+  return (
+    <>
+      <a href='https://kartverket.no' aria-label='Kartverket'>
+        <img src={logoFull} className='header-logo header-logo--full' aria-hidden />
+        <img src={logoIcon} className='header-logo header-logo--icon' aria-hidden />
+      </a>
+
+      {applicationName && (
+        <>
+          <span className='header-brand-divider' />
+          {applicationHref ? (
+            <Link href={applicationHref}>
+              <Heading data-size='2xs'>{applicationName}</Heading>
+            </Link>
+          ) : (
+            <Heading data-size='2xs'>{applicationName}</Heading>
+          )}
+        </>
+      )
+      }
+    </>
+  );
+}
+
+function InternalBrand({ applicationName, applicationHref }: InternalBrandProps) {
+  return (
+    <>
+      <Link
+        href={applicationHref}
+        className='header-brand--internal'
+      >
+        <img
+          src={logoIcon}
+          className='header-logo header-logo--icon'
+          aria-hidden
+        />
+        <Heading data-size='2xs'>{applicationName}</Heading>
+      </Link>
+    </>
+  );
+}
