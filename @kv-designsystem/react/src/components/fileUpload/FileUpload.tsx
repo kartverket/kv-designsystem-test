3ecfb58bf1ccdcd2a844da60a66a8ac5d0f8f13c
zwecla <clara.zweigel@kartverket.no>
2026-08-24 12:01:41 +0200

    feat: add new component fileupload (#55)

---
+++ @kv-designsystem/react/src/components/fileUpload/FileUpload.tsx
@@ -0,0 +1,6 @@
+import { EXPERIMENTAL_FileUpload as FileUpload, type FileUploadProps } from '@digdir/designsystemet-react';
+
+FileUpload.displayName = 'FileUpload';
+
+export type { FileUploadProps };
+export { FileUpload };
