170340e9c1f049d4f691fea73d602f27fac28e28
Clara Zweigel <clara.zweigel@kartverket.no>
2026-08-27 12:31:04 +0200

    fix: update header dialog trigger

--- @kv-designsystem/react/src/components/header/HeaderSearchButton.tsx
+++ @kv-designsystem/react/src/components/header/HeaderSearchButton.tsx
@@ -1,26 +1,29 @@
 import {
-	forwardRef,
-	type ForwardRefExoticComponent,
-	type RefAttributes,
+  forwardRef,
+  type ForwardRefExoticComponent,
+  type RefAttributes,
 } from 'react';
-import type { ButtonProps } from '../button/Button';
 import { MagnifyingGlassIcon } from '@navikt/aksel-icons';
-import { HeaderDialogTrigger } from './HeaderDialogTrigger';
+import { HeaderDialogTrigger, HeaderDialogTriggerProps } from './HeaderDialogTrigger';
 
-export type HeaderSearchButtonProps = ButtonProps;
+// Omit `primaryIcon` because this component always uses `MagnifingGlassIcon`.
+// Omit `label` to redfine as optional. 
+export type HeaderSearchButtonProps = Omit<HeaderDialogTriggerProps, 'primaryIcon' | 'label'> & {
+  label?: string;
+};
 
 export const HeaderSearchButton: ForwardRefExoticComponent<
   HeaderSearchButtonProps & RefAttributes<HTMLButtonElement>
 > = forwardRef<HTMLButtonElement, HeaderSearchButtonProps>(
-  function HeaderSearchButton( { className, commandFor='search-dialog', ...rest}, ref) {
+  function HeaderSearchButton({ className, commandFor = 'search-dialog', label = 'Søk', ...rest }, ref) {
     return (
-      <HeaderDialogTrigger 
-      className={className}
-      commandFor={commandFor}
-      label='Søk'
-      primaryIcon={MagnifyingGlassIcon}
-      ref={ref}
-      {...rest}
+      <HeaderDialogTrigger
+        className={className}
+        commandFor={commandFor}
+        label={label}
+        primaryIcon={MagnifyingGlassIcon}
+        ref={ref}
+        {...rest}
       />
     );
   }
